4cb1aee480ef47a153ecdc831c9ce82ca072f642
Alexia Raphaela <alexiaraphaela@gmail.com>
2020-05-12 13:39:48 -0300

    Adding a JavaScript function that draw an object

---
+++ sprite.js
@@ -0,0 +1,12 @@
+function Sprite(x, y, largura, altura) {
+    this.x = x;
+    this.y = y;
+    this.largura = largura;
+    this.altura = altura;
+
+    this.desenha = function(xCanvas, yCanvas) {
+        ctx.drawImage();
+
+    }
+
+}
